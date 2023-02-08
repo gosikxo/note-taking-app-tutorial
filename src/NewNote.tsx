d5918229d91fff02e2a37a00011eea6fe85e3f86
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-08 19:02:15 +0100

    Add note form

---
+++ src/NewNote.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NoteForm } from './NoteForm'
+
+export const NewNote = () => {
+    return (
+        <>
+            <h1 className='mb-4'>New Note</h1>
+            <NoteForm />
+        </>
+    )
+}
