d2d028318f5070e96c523b6802f786f9e2f2d7df
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-09 18:49:10 +0100

    Handle submitting form using useRef

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { NewNote } from './NewNote';
 
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 function App() {
   return (
     <Container className='my-4'>
